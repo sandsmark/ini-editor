25a904ffefcbe47e8e87a4b1aba4e71aac24480a
Martin T. H. Sandsmark <sandsmark@samfundet.no>
2012-04-27 02:24:52 +0200

    add warning dialog if no ini dialog supplied

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <QtGui/QApplication>
+#include <QMessageBox>
 #include "dialog.h"
 
 int main(int argc, char *argv[])
 {
-    if (argc < 2)
-        return -1;
     QApplication a(argc, argv);
+    if (argc < 2) {
+        QMessageBox::critical(0, "No ini file supplied!", "Please supply an ini file to open as first argument.");
+        return -1;
+    }
+
     Dialog w(qApp->arguments().at(1));
     w.show();
     
